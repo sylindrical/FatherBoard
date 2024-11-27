3aa887caebf3a0058515394d3e4022998ce0ec83
sylindrical <turtlerbean@protonmail.com>
2024-11-27 20:34:12 +0000

    Some styling on product boxes

--- Fatherboard/resources/views/home.blade.php
+++ Fatherboard/resources/views/home.blade.php
@@ -4,24 +4,28 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href={{asset("/css/home.css")}} />
     <title>Home</title>
 </head>
 <body>
     <h2>Home Page</h2>
 
 <br>
+<div id="ProductContainer">
     <?php
     foreach($data as $x=>$item)
     {
 
             ?>
+            <div class="ProductItem">
             <h2><?php echo $item['Title'] ?></h2>
             <p><?php echo $item['Description'] ?></p>
             <p><?php echo $item['Owner'] ?></p>
-
+            </div>
             <?php
 
     }
     ?>
+    </div>
 </body>
 </html>
