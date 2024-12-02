e36167e1f0034568821dfc9f5abcf2e705d266f2
Ahsen-afk <230059672@aston.ac.uk>
2024-12-02 20:45:59 +0000

    Revert "Merge branch 'front-end-conversion'"
    This reverts commit 4a8cfde5d50bb1777acee65f2243a2bf2ccd8500, reversing
    changes made to b9a4b1146665dfd2e41e2db479f0ee3edddaa5c0.

--- Fatherboard/resources/views/login.blade.php
+++ Fatherboard/resources/views/login.blade.php
@@ -1,54 +1,20 @@
-<head> 
-    <meta charset="utf-8" />
-    <title>Login Page</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel = "stylesheet" type="text/css" href={{asset("css/stylesheet.css")}} /><!-- Link for header  stylesheet -->
-    <link rel = "stylesheet" type="text/css" href={{asset("css/logincss.css")}} /><!-- Link for the login styles -->
-</head>
 
-   <!-- Header Section (unchanged, linked to stylesheet.css) -->
-<header class="main-header">
-  <div class="container">
-      <a href="home.php"><img src={{asset("images/FatherboardTransparentCrop.png")}} id="logo" alt="FatherBoard Logo" width="100" height="50"></a>
-      <form class="SearchBar">
-          <input type="text" placeholder="Search.." name="search">
-      </form>
-      <nav class="main-nav">
-          <ul class="main-nav-list">
-              <li><a href="#register">About Us</a></li>
-              <li><a href="#login" class = "active">Account</a></li>
-              <li><a href="#products">Basket</a></li>
-          </ul>
-      </nav>
-  </div>
-  <div class="container">
-      <nav class="lower-nav">
-          <ul class="lower-nav-list">
-              <li>
-                  <a href="#product1">Memory</a>
-                  <a href="#product2">CPUs</a>
-                  <a href="#product3">Prebuilt Computers</a>
-                  <a href="#product4">GPUs</a>
-                  <a href="#product5">PSUs</a>
-                  <a href="#sale">Sale!!!</a>
-              </li>
-          </ul>
-      </nav>
-  </div>
-</header>
+<x-layout>
+    <x-slot:title>
+        s
+    </x-slot:title>
+    <x-slot:sheet>
+        login
+    </x-slot:sheet>
+    <h2>Log in</h2>
 
-   <body>
-    <!-- Login page form -->
-    <header id = "header1">
-        <h1>Login</h1>
-        <p>New user? <a href="Signup.html"><p>Create a new account!</p></a></p>
-        <form id = "login">
-            <br> <input type = "email" id ="mail" placeholder="Email" required/> </br>
-            <br> <input type="password" id ="pass" placeholder="Password" required /> </br>
-            <div id="pass2">
-            <a href="code if user forget password">Forgot password?</a> <!-- ADD CODE HERE -->
-            </div id="pass2">
-            <br> <input type="submit" id ="lgnbtn" value="LOGIN" /> </br>
-      </form>
-      </header>
-   </body>
+    <form action="./_login" method="POST">
+        @csrf
+        <input type="text" name="username">
+        <input type="text" name="password">
+        <input type="checkbox" name="permanent">
+        <input type="submit" name="submit">
+    </form>
+
+    <p></p>
+</x-layout>
--- Fatherboard/resources/views/register.blade.php
+++ Fatherboard/resources/views/register.blade.php
@@ -1,58 +1,17 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8" />
-    <title>Signup Page</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel = "stylesheet" type="text/css" href={{asset("css/stylesheet.css")}} /><!-- Link for header  stylesheet -->
-    <link rel = "stylesheet" type="text/css" href={{asset("css/logincss.css")}} /> <!-- Link for the header styles -->
-</head>
+<x-layout>
+    <x-slot:title>
+        Register
+    </x-slot:title>
+    <x-slot:sheet>
+        register
+    </x-slot:sheet>
+    <h2>Register</h2>
 
-<!-- Header Section (unchanged, linked to stylesheet.css) -->
-<header class="main-header">
-    <div class="container">
-        <a href="home.php"><img src={{asset("images/FatherboardTransparentCrop.png")}} id="logo" alt="FatherBoard Logo" width="100" height="50"></a>
-        <form class="SearchBar">
-            <input type="text" placeholder="Search.." name="search">
-        </form>
-        <nav class="main-nav">
-            <ul class="main-nav-list">
-                <li><a href="#register">About Us</a></li>
-                <li><a href="#login" class = "active">Account</a></li>
-                <li><a href="#products">Basket</a></li>
-            </ul>
-        </nav>
-    </div>
-    <div class="container">
-        <nav class="lower-nav">
-            <ul class="lower-nav-list">
-                <li>
-                    <a href="#product1">Memory</a>
-                    <a href="#product2">CPUs</a>
-                    <a href="#product3">Prebuilt Computers</a>
-                    <a href="#product4">GPUs</a>
-                    <a href="#product5">PSUs</a>
-                    <a href="#sale">Sale!!!</a>
-                </li>
-            </ul>
-        </nav>
-    </div>
-  </header>
+    <form action="./_register" method="POST">
+        @csrf
+        <input type="text" name="username">
+        <input type="text" name="password">
 
-<body>
-
-    <!-- Sign up form -->
-    <header id = "header2">
-        <h1>Create Account</h1>
-        <p>Current User? <a href="Login.html"><p>Login!</p></a></p>
-        <form id = "signup">
-            <br> <input type = "email" id ="mail2" placeholder="Email" required/> </br>
-            <br> <input type="text" id="fname" placeholder="First Name" required/> </br>
-            <br> <input type="text" id="lname" placeholder="Last Name" required/> </br>
-            <br> <input type="password" id ="spass" placeholder="Password" required /> </br>
-            <br> <input type="password" id="spass2" placeholder="Confirm Password" required/> </br>
-            <br> <input type="checkbox" id="check" required/> <label for="check"> <a href="terms.html">Agree to Terms and Conditions</a></label> </br>
-            <br> <input type="submit" id ="sbtn" value="CREATE ACCOUNT" /> </br>
-        </form>
-    </header>
-</body>
+        <input type="submit" name="submit">
+    </form>
+</x-layout>
