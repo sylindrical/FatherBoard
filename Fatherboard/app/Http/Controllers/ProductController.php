4e86d20e1b293c9a00db42cc3348beaf5d0bb158
SaMEer27-10 <smrmahmood01@gmail.com>
2024-12-04 10:37:32 +0000

    updating the public function index

--- Fatherboard/app/Http/Controllers/ProductController.php
+++ Fatherboard/app/Http/Controllers/ProductController.php
@@ -11,9 +11,9 @@ class ProductController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Product $product)
     {
-        
+        return view('product', ["product"=>$product]);
     }
 
     /**
