d218606096664891690e9a35ee770d243c889292
romistry19 <romistry10@gmail.com>
2024-12-05 15:15:19 +0000

    got rid of the authentication better adjusted the code to website needs

--- Fatherboard/app/Http/Controllers/BasketController.php
+++ Fatherboard/app/Http/Controllers/BasketController.php
@@ -12,71 +12,54 @@ class BasketController extends Controller
         $quantity = $request->input('quantity', 1);
 
 // make it so that if a user is logged in it will save basket
+        $basket = session()->get('basket',[]);
 
-            $basket = json_decode($user->basket->items ?? '[]', true);
-            $basket[$productId] = [
-                'product_id' => $productId,
-                'quantity' => ($basket[$productId]['quantity'] ?? 0) + $quantity,];
 
-            $user->basket()->updateOrCreate([],['items'=>json_encode($basket)]);
+                $basket = [$productId]=['product_id' => $productId, 'quantity' => ($basket[$productId]['quantity'] ?? 0) + $quantity,];
 
-            $basket = session()->get('basket',[]);
-            $basket[$productId] = [
-                'product_id' => $productId,
-                'quantity' => ($basket[$productId]['quantity'] ?? 0) + $quantity,];
 
             session()->put('basket',$basket);
-        }
+
     return redirect()->route('basketIndex')->with(['success','Product added!']);
     }
 
 //display the basket
     public function index(){
 
-        $basket = auth()->check()
-       ? json_decode(auth()->user()->basket->items ?? '[]', true) : session()->get('basket',[]);
-
-        return view('basket', compact('basket'));
+        $basket = session()->get('basket',[]);
+        return view('basket',compact('basket'));
         }
 
 
         public function update(Request $request){
             $productId = $request->input('product_id');
             $quantity = $request->input('quantity');
-            if (auth()->check()){
-                $user =auth()->user();
-                $basket = json_decode($user->basket->items ?? '[]', true);
+
+                $basket = session()->get('basket',[]);
                 if(isset($basket[$productId])){
                     $basket[$productId]['quantity'] = $quantity;
                 }
-                $user->basket()->updateOrCreate([], ['items'=> json_encode($basket)]);
-            }else{
-$basket = session()->get('basket',[]);
-if(isset($basket[$productId])){
-    $basket[$productId]['quantity'] = $quantity;
-}
+
 session()->put('basket',$basket);
-}
+
 return redirect()->route('basketIndex')->with('success','Basket Updated!');
         }
 
 
         public function remove(Request $request){
-            $basket = Session::get('basket',[]);
+
             $productId = $request->input('product_id');
+            $basket=session()->get('basket',[]);
             if (isset($basket[$productId])){
                 unset($basket[$productId]);
             }
-            Session::put('basket',$basket);
+            Session()->put('basket',$basket);
             return redirect()->route('basketIndex')->with('success','Product removed!');
         }
         public function checkout(){
-            if (auth()->check()){
+
                 return view('checkout');
 
-            }else{
-                return redirect()->route('register')->with('Info', 'Please create an account to continue');
-            }
         }
 }
 
