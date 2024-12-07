e22ecfed246ad683277e164ffeee7559d1974985
sylindrical <turtlerbean@protonmail.com>
2024-12-07 15:18:15 +0000

    blade refactoring

--- Fatherboard/resources/views/contact.blade.php
+++ Fatherboard/resources/views/contact.blade.php
@@ -11,39 +11,7 @@
 </x-slot:head>
 
     <!-- Header Section (unchanged, linked to stylesheet.css) -->
-    <header class="main-header">
-        <div class="container">
-            <a href="Home.html">
-                <img src="{{asset('images/FatherboardTransparentCrop.png')}}" id="logo" alt="FatherBoard Logo" width="100" height="50">
-            </a>
-
-            <form class="SearchBar">
-                <input type="text" placeholder="Search.." name="search">
-            </form>
-
-            <nav class="main-nav">
-                <ul class="main-nav-list">
-                    <li><a href="About.html">About Us</a></li>
-                    <li><a href="Login.html">Account</a></li>
-                    <li><a href="Basket.html">Basket</a></li>
-                </ul>
-            </nav>
-        </div>
-        <div class="container">
-            <nav class="lower-nav">
-                <ul class="lower-nav-list">
-                    <li>
-                        <a href="#product1">Memory</a>
-                        <a href="Products.html#CPUs">CPUs</a>
-                        <a href="#product3">Prebuilt Computers</a>
-                        <a href="#product4">GPUs</a>
-                        <a href="#product5">PSUs</a>
-                        <a href="#sale">Sale!!!</a>
-                    </li>
-                </ul>
-            </nav>
-        </div>
-    </header>
+    <x-header></x-header>
 
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
