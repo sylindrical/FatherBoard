a500ea54fabb9d949d8c3b0972c19b320e871143
romistry19 <romistry10@gmail.com>
2024-12-08 19:56:21 +0000

    Amendments for basket database

--- Fatherboard/app/Http/Controllers/BasketController.php
+++ Fatherboard/app/Http/Controllers/BasketController.php
@@ -14,19 +14,23 @@ class BasketController extends Controller
 
 $product = Product::findOrFail($productId);
 
-        $basket = session()->get('basket',[]);
-if(isset($basket[$product->id])){
-    $basket[$product->id]['quantity']+=$quantity;
+        $basket = Basket::firstOrCreate(['user_id' => $user->id]);
+
+$items = json_decode($basket->items, true) ?? [];
+
+if(isset($items[$product->id])){
+    $items[$product->id]['quantity']+=$quantity;
 }else{
 
-                $basket[$product->id]=[
+                $items[$product->id]=[
                 'product_id' => $product->id,
                 'name' => $product->Title,
                 'price'=> $product->Price->price,
                 'quantity' => $quantity,];
                 }
 
-            session()->put('basket',$basket);
+            $basket->items = json_encode($items);
+            $basket->save();
 
     return redirect()->route('basketIndex')->with(['success','Product added!']);
     }
