6b619f0ca773a2da4d59a6856a67be8e50709202
sylindrical <turtlerbean@protonmail.com>
2024-12-08 16:36:07 +0000

    made home page not be auth-protected

--- Fatherboard/app/Http/Controllers/HomeController.php
+++ Fatherboard/app/Http/Controllers/HomeController.php
@@ -8,14 +8,17 @@ class HomeController extends Controller
 {
     public static function giveHome()
     {
-        if (AuthController::loggedIn())
-        {
-            $product = Product::all();
-            return view("home",["data"=>$product]);
-        }
-        else
-        {
-            return redirect("./login");
-        }
+        // if (AuthController::loggedIn())
+        // {
+        //     $product = Product::all();
+        //     return view("home",["data"=>$product]);
+        // }
+        // else
+        // {
+        //     return redirect("./login");
+        // }
+
+        $product = Product::all();
+        return view("home",["data"=>$product]);
     }
 }
