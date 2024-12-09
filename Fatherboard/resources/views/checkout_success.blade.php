26925a1ef213613628f2503affe64d7281e2f452
Benjamin Lee <benlee007@live.co.uk>
2024-12-09 11:07:12 +0000

    Added frontend to checkout complete

--- Fatherboard/resources/views/checkout_success.blade.php
+++ Fatherboard/resources/views/checkout_success.blade.php
@@ -1,4 +1,12 @@
-<x-layout>
+<x-lowlayout>
+    <x-slot:head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>FatherBoard - Checkout</title>
+        <link rel="stylesheet" type="text/css" href="{{asset('css/aboutus.css')}}"> <!-- Link for the header styles -->
+        <link rel="stylesheet" type="text/css" href="{{asset('css/checkout.css')}}"> <!-- Link for homepage-specific styles -->
+    </x-slot:head>
+    <x-header></x-header>
     <x-slot:title>
         success
     </x-slot:title>
@@ -6,5 +14,4 @@
         Checkout Complete
     </x-slot:sheet>
     <h2>Checkout Complete</h2>
-    </form>
-</x-layout>
+</x-lowlayout>
