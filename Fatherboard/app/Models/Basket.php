ace9cb1a830100a08a365c59b68d9908c40d397b
romistry19 <romistry10@gmail.com>
2024-12-09 00:05:03 +0000

    a

--- Fatherboard/app/Models/Basket.php
+++ Fatherboard/app/Models/Basket.php
@@ -25,6 +25,12 @@ public function product(){
 public function productPrice(){
     return $this->hasOne(ProductPrice::class,'product_id','product_id');
 }
-
-
+public function customerInformation(){
+    return $this->belongsTo(CustomerInformation::class,'customer_information_id');
+}
+public function getProductsAttribute()
+{
+    $productIds = collect($this->items)->pluck('product_id')->toArray();
+    return Product::whereIn('id', $productIds)->get();
+}
 }
--- Fatherboard/app/Models/User.php
+++ Fatherboard/app/Models/User.php
@@ -45,4 +45,8 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+    public function customerInformation()
+{
+    return $this->hasOne(CustomerInformation::class, 'user_id');
+}
 }
